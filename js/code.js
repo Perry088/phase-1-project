17959088e4384852a143256a096a80182f3d582f
Trenton Perry <trentperry08@gmail.com>
2023-10-27 13:37:48 -0800

    added the promise for the fetch

--- js/code.js
+++ js/code.js
@@ -1,3 +1,11 @@
 const postContainer = document.getElementById('post-container')
 
 fetch('http://localhost:3000/dishes')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => {
+            const postElement = createPostElement(post);
+            postContainer.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error("Error fetching data:", error));
