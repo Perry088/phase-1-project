8f6b12e7696040574308b682fd17236ed1662c9b
Trenton Perry <trentperry08@gmail.com>
2023-10-27 14:04:46 -0800

    made create element for dish name

--- js/code.js
+++ js/code.js
@@ -18,4 +18,11 @@ fetch('http://localhost:3000/dishes')
         postImage.src = post.image;
         postImage.alt = 'Post Image';
         postImage.className = 'post-image';
+        
+        const postContent = document,createElement('div');
+        postContent. className = 'post-content';
+
+        const postName = document.createElement('h1');
+        postName.textContent = post.name;
+        postName.className = 'post-name';
     }
